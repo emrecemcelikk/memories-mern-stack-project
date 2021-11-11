e9f7faceb386f3527e6828dc5c860a8eb52bac46
Emre Cem Celik <58655850+emrecemcelikk@users.noreply.github.com>
2021-11-11 11:12:03 +0300

    second commit

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,13 +1,33 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect} from 'react';
 
 import { TextField,Button,Typography,Paper } from '@material-ui/core';
 import FileBase from 'react-file-base64';
-import useStyles from './style';
 
-const Form =()=>{
+import {useDispatch} from 'react-redux';
+import { useSelector } from 'react-redux';
+import useStyles from './style';
+import { createPost } from '../../actions/posts'
+import { updatePost } from '../../actions/posts'
+
+const Form =( {currentId, setCurrentId})=>{
     const [postData,setPostData] = useState({creator:'',title:'',message:'',tags:'',selecetedFile:''})
+    const post = useSelector((state)=> currentId? state.posts.find((p)=>p._id===currentId): null);
     const classes= useStyles();
-    const handleSubmit =()=>{
+    const dispatch = useDispatch();
+
+    useEffect(()=>{
+        if(post) setPostData(post);
+    },[post])
+
+    const handleSubmit =(e)=>{
+        e.preventDefault();
+
+        if(currentId) {
+            dispatch(updatePost(currentId,postData));
+        }
+        else {
+            dispatch(createPost(postData));
+        }
 
     }
     
@@ -21,18 +41,18 @@ const Form =()=>{
                 <Typography variant="h6">Create a Memory</Typography>
                 <TextField name="creator" variant="outlined"
                 label="Creator" fullWidth value={postData.creator}
-                onChange={(e)=> setPostData({creator: e.target.value})}/>
+                onChange={(e)=> setPostData({...postData, creator: e.target.value})}/>
                 <TextField name="title" variant="outlined"
                 label="Title" fullWidth value={postData.title}
-                onChange={(e)=> setPostData({title: e.target.value})}/>
+                onChange={(e)=> setPostData({...postData, title: e.target.value})}/>
                 <TextField name="message" variant="outlined"
                 label="Message" fullWidth value={postData.message}
-                onChange={(e)=> setPostData({message: e.target.value})}/>
+                onChange={(e)=> setPostData({...postData, message: e.target.value})}/>
                 <TextField name="tags" variant="outlined"
                 label="Tags" fullWidth value={postData.tags}
-                onChange={(e)=> setPostData({tags: e.target.value})}/>
+                onChange={(e)=> setPostData({...postData,tags: e.target.value})}/>
                 <div className={classes.fileInput}>
-                    <FileBase type="file" multiple={false} onDone={({base64})=> setPostData({...postData,selecetedFile: base64})} />
+                    <FileBase type="file" multiple={false} onDone={({base64})=> setPostData({...postData, selecetedFile: base64})} />
                 </div>
                 <Button className={classes.buttonSubmit} variant="container" color="primary" size="large" type="submit" fullWidth >Submit</Button>
                 <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth >Clear</Button>
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,9 +1,12 @@
+// eslint-disable-next-line import/no-anonymous-default-export
 export default (posts=[],action) => {
     switch (action.type) {
         case 'FETCH_ALL':
             return action.payload;
         case 'CREATE':
-            return posts;
+            return posts.map((post)=>post._id === action.payload._id ? action.payload : post);
+        case 'UPDATE':
+            return [...posts, action.payload];
         default:
             return posts;
     }
